b326ad60a2fa3886000ef7105024336a560746f4
Bo (Hawk) Wang <143765135+hawkrsrch@users.noreply.github.com>
2025-03-17 11:19:31 -0500

    Update example_LLM_API_call.py

--- example_LLM_API_call.py
+++ example_LLM_API_call.py
@@ -4,76 +4,69 @@ API_KEY = ""
 
 import openai
 
-client = openai.Client(base_url=f"http://{HOST}:{PORT}/v1", api_key=API_KEY)
-model_name = client.models.list().data[0].id
+class local_llm_api:
+    def __init__(self, host_ip, port_num, api_key_string):
+        self.client = openai.Client(base_url=f"http://{host_ip}:{port_num}/v1", api_key=api_key_string)
+        self.model_name = self.client.models.list().data[0].id
 
-print(model_name)
+        self.history = []
+
+        # print model name when doing initialization
+        print(self.model_name)
+
+    def get_reponse(self, message):
+
+        self.history.append({
+            "role": "user",
+            "content": message,
+        })
+
+        response_stream = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=self.history,
+            temperature=0.6,
+            top_p=0.95,
+            stream=True,
+        )
+
+        reasoning = ""
+        answer = ""
+        content_start = False
+        reason_start = False
+        for chunk in response_stream:
+            if chunk.choices[0].delta.content:
+                if not content_start:
+                    content_start = True
+                    print("\n==== End of the reasoning ====")
+                    print("\n==== Beginning of the answer ====")
+                print(chunk.choices[0].delta.content, end="")
+                answer += chunk.choices[0].delta.content
+            elif chunk.choices[0].delta.reasoning_content:
+                if not reason_start:
+                    reason_start = True
+                    print("\n==== Beginning of the reasoning ====")
+                print(chunk.choices[0].delta.reasoning_content, end="")
+
+        print("\n==== End of the answer ====\n")
+
+        return answer
+
+    def append_history(self, answer):
+
+        self.history.append({
+            "role": "assistant",
+            "content": answer,
+        })
+
+
+# example
+call_llm = local_llm_api(HOST, PORT, API_KEY)
 
 # message = "How many 'r's are in the word 'strawberry'?"
 message = "What are the top 3 most common misconceptions about large language models?"
 message2 = "How can these misconceptions be addressed or corrected?"
 
-history = [
-    {
-        "role": "user",
-        "content": message,
-    }
-]
+answer = call_llm.get_reponse(message)
+call_llm.append_history(answer)
 
-response_stream = client.chat.completions.create(
-    model=model_name,
-    messages=history,
-    temperature=0.6,
-    top_p=0.95,
-    stream=True,  # Non-streaming
-    # extra_body={"separate_reasoning": True},
-)
-
-reasoning = ""
-answer = ""
-content_start = False
-for chunk in response_stream:
-    if chunk.choices[0].delta.content:
-        if not content_start:
-            content_start = True
-            print("\n==== Text ====")
-        print(chunk.choices[0].delta.content, end="")
-        answer += chunk.choices[0].delta.content
-    elif chunk.choices[0].delta.reasoning_content:
-        print(chunk.choices[0].delta.reasoning_content, end="")
-
-print("\n==== End of the answer ====")
-
-history.append({
-    "role": "assistant",
-    "content": answer,
-})
-
-# New question
-history.append({
-    "role": "user",
-    "content": message2,
-})
-
-# Send out new request
-response_stream = client.chat.completions.create(
-    model=model_name,
-    messages=history,
-    temperature=0.6,
-    top_p=0.95,
-    stream=True,  # Non-streaming
-    # extra_body={"separate_reasoning": True},
-)
-
-reasoning = ""
-answer = ""
-content_start = False
-for chunk in response_stream:
-    if chunk.choices[0].delta.content:
-        if not content_start:
-            content_start = True
-            print("\n==== Text ====")
-        print(chunk.choices[0].delta.content, end="")
-        answer += chunk.choices[0].delta.content
-    elif chunk.choices[0].delta.reasoning_content:
-        print(chunk.choices[0].delta.reasoning_content, end="")
+call_llm.get_reponse(message2)
